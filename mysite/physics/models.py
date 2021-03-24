91fff27b8aabcd5c62478a3d718da273237903c1
Xosimo <cosimofu@gmail.com>
2021-03-24 21:13:43 +0800

    Delete the useless 'login' and 'signup' buttons.

--- mysite/physics/models.py
+++ mysite/physics/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 class Projects(models.Model):
-    name = models.CharField(max_length=200, primary_key = True)
+    name = models.CharField(max_length=200, primary_key=True)
     html_path = models.CharField(max_length=200, null=True)
     pic_path = models.CharField(max_length=200, null=True)
-    
+
+
 class UploadProjects(models.Model):
     name = models.CharField(max_length=200, primary_key = True)
     path = models.CharField(max_length=200)
+
     def add(self):
         projects = Projects(name=self.name, path=self.path)
         projects.save()
