04adf6b3d60be92ea659b3c4102c7a67d5a4222c
hhd88888 <674446990@qq.com>
2021-04-23 18:22:29 +0800

    add comments and merge server

--- server/.gitignore/accounts/templatetags/widget_tweaks.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def add_class(form_widget, css_class):
-	""" Adds a css class to a django form widget """
-	return form_widget.as_widget(attrs={'class': css_class})
