531a94be78328953cdeb7d1fc738b544146880a2
Xosimo <cosimofu@gmail.com>
2021-04-25 09:31:49 +0800

    Add hint at the upload page.

--- fdu-physlet/physics/models.py
+++ fdu-physlet/physics/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
+def validate_file_extension(value):
+    if not value.name.endswith('.zip'):
+        raise ValidationError(u'Please Upload Zip Files!')
 
 
 class Project(models.Model):
@@ -8,7 +13,7 @@ class Project(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='projects', on_delete=models.CASCADE)
     title = models.CharField(max_length=128, blank=True)
     thumbnail = models.ImageField(upload_to='thumbnails', blank=True)
-    upload_file = models.FileField(upload_to='projects/zipfiles')
+    upload_file = models.FileField(upload_to='projects/zipfiles', validators=[validate_file_extension])
     upload_date = models.DateTimeField(auto_now_add=True)
     is_reviewed = models.BooleanField(default=False)
     index_path = models.CharField(max_length=512)
